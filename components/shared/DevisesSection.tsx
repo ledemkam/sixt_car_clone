adcb0dc3e1faf81a906245a64192c8f8a653b286
ledemkam <ledemkam@gmail.com>
2024-04-15 21:13:52 +0200

    start_klein_projet

---
+++ components/shared/DevisesSection.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DevisesSection() {
+  return (
+    <div>
+      devise
+    </div>
+  )
+}
---
+++ components/shared/SloganSection.tsx
@@ -0,0 +1,3 @@
+export default function SloganSection() {
+  return <div>Slogan</div>;
+}
